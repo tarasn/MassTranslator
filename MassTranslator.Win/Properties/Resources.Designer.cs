31561817e87257bf3d173b24fd08bcad9aa6fcce
tarasn <tarasn@gmail.com>
2017-10-29 12:05:04 +0200

    Changed default application icon

--- MassTranslator.Win/Properties/Resources.Designer.cs
+++ MassTranslator.Win/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace MassTranslator.Win.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon MassTranslator_2_128x128 {
+            get {
+                object obj = ResourceManager.GetObject("MassTranslator_2_128x128", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
